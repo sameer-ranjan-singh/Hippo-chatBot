60f6a2abe42ed7b3fd13c58cb2647e98a5cb6b32
Sameer Ranjan Singh <sameer.lancer99@gmail.com>
2023-04-18 22:10:15 +0530

    UPDATE : fetch

--- client/script.js
+++ client/script.js
@@ -94,7 +94,7 @@ const handleSubmit = async (e) => {
 
     //fetch data from server : bot's response
     
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://hippo-chatbot.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
