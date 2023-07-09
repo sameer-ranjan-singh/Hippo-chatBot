382da3b34cf5b981a5748f9aba7e089f41c98df4
Sameer Ranjan Singh <sameer.lancer99@gmail.com>
2023-07-09 15:42:57 +0530

    install : axios

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai'
-
+import axios from 'axios'
 dotenv.config()
 
 
